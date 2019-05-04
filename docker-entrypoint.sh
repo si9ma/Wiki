f72506a3b0b9037c2e1c32f53c3e0f196c6f5359
si9ma <hellob374@gmail.com>
2019-05-04 16:20:02 +0800

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -31,9 +31,9 @@ chown -R www-data:www-data /var/www/
 [ "$repo" = "" ] && repo="https://github.com/si9ma/Wiki.git"
 git_repo=`echo $repo | sed 's/https:\/\/github.com\//git@github.com:/g'`
 rm -rf Wiki # remove Wiki git repo
-sudo -u www-data git clone $repo -o Wiki && cd Wiki && git remote rename Wiki origin && git remote set-url origin $git_repo # run as user www-data
 
 # run as user www-data
+sudo -u www-data bash -c "git clone $repo -o Wiki && cd Wiki && git remote rename Wiki origin && git remote set-url origin $git_repo" # run as user www-data
 sudo -u www-data bash -c 'ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N "" && cat ~/.ssh/id_rsa.pub' # copy public key to github
 sudo -u www-data bash -c 'ssh-keyscan github.com >> ~/.ssh/known_hosts'
 sudo -u www-data bash -c 'git config user.email "$email"'
